966097d0d82a172e249c788aecbb68af505aced1
Gerald Bauer <gerald.bauer@gmail.com>
2015-08-17 23:24:19 +0200

    up

--- lib/quik/version.rb
+++ lib/quik/version.rb
@@ -3,8 +3,8 @@
 module Quik
 
   MAJOR = 0    ## todo: namespace inside version or something - why? why not??
-  MINOR = 1
-  PATCH = 0
+  MINOR = 0
+  PATCH = 1
   VERSION = [MAJOR,MINOR,PATCH].join('.')
 
   def self.version
